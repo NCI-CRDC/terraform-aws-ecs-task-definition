03afa9f53573c9d87070a792e787fcb6bb70c4cd
Cole DeVries <colemandevries@gmail.com>
2023-02-04 14:24:47 -0500

    updated the task definition

--- variables.tf
+++ variables.tf
@@ -14,6 +14,7 @@ variable "env" {
     error_message = "valid values are 'dev', 'qa', 'stage', 'prod', and 'nonprod'"
   }
 }
+
 variable "program" {
   type        = string
   description = "the program associated with the application"
@@ -23,4 +24,40 @@ variable "program" {
     condition     = contains(["crdc", "ccdi", "ctos"], var.program)
     error_message = "valid values for program are 'crdc', 'ccdi', and 'ctos'"
   }
+}
+
+variable "cpu" {
+  type        = number
+  description = ""
+}
+
+variable "execution_role_arn" {
+  type        = string
+  description = ""
+}
+
+variable "memory" {
+  type        = number
+  description = ""
+}
+
+variable "network_mode" {
+  type        = string
+  description = ""
+  default     = "awsvpc"
+}
+
+variable "requires_compatibilities" {
+  type        = set(string)
+  description = ""
+  default     = ["FARGATE"]
+}
+
+variable "resource_name_suffix" {
+  type = string
+}
+
+variable "task_role_arn" {
+  type        = string
+  description = ""
 }
